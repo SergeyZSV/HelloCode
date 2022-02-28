96f55b2b72b17a4b7f494ca8b33b9e3391b0cf3d
Sergey <sergey06062001@gmail.com>
2022-03-01 00:30:00 +0300

    random

--- Example003_Number/Program.cs
+++ Example003_Number/Program.cs
@@ -1,3 +1,5 @@
-﻿int a = 3;
-int b = 5;
+﻿int a = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9 
+Console.WriteLine(a);
+int b = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9 
+Console.WriteLine(b);
 Console.WriteLine(a + b);
