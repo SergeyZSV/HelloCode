68e36b1f5e22ef6416c90d5ca3a36d3dc9f015a2
Sergey <sergey06062001@gmail.com>
2022-03-01 00:43:30 +0300

    if else

---
+++ Example005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write Username: ");
+string user = Console.ReadLine();
+
+if(user.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, thats Masha");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(user);
+}
